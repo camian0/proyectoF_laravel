1cb258789e15f3836126c256b549db0b410606db
camian0 <camian6@outlook.com>
2022-10-19 16:29:48 -0500

    template base and index were added

---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+
+
+    <title>@yield('title')</title>
+</head>
+
+<body>
+
+    <div class="row">
+        <div class="col-sm-12 col-md-1  col-lg-1"></div>
+        <div class="col-sm-12 col-md-10 col-lg-10">
+            @yield('container')
+        </div>
+        <div class="col-sm-12 col-md-1  col-lg-1"></div>
+    </div>
+
+
+</body>
+
+</html>
---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.base')
+@section('title', 'Lista de productos')
+@section('container')
+    <h1>lista de productos</h1>
+@endsection
