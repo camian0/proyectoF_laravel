1b90d0c085533f4b1675b62c0c2ce2ae6465cea3
camian0 <camian6@outlook.com>
2022-10-29 22:30:21 -0500

    image to show on template was modified, also the index method

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function __construct()
+    {
+
+    }
+
+    const IMAGES = [
+        'https://naturcol.com/wp-content/uploads/2020/02/COLAGENO-x-100.jpg',
+        'https://periodismopublico.com/wp-content/uploads/2020/06/Vitalnip_Soacha.jpg',
+        'https://todorganic.com/wp-content/uploads/2019/08/moringa-11-1-500x500.jpg',
+        'https://cdn.shopify.com/s/files/1/2316/1373/products/calendula-x-50-capsulas-932582_large.jpg?v=1623951120',
+        'https://cdn.shopify.com/s/files/1/2316/1373/products/jalea-real-con-mangostino-x-300-gr-367282_large.jpg?v=1623951470',
+        'https://cdn.shopify.com/s/files/1/2316/1373/products/bon-star-valeriana-x-100-caps-958702_large.jpg?v=1623951141',
+        'https://s1.eestatic.com/2015/11/23/cocinillas/cocinillas_81502050_116226012_640x480.jpg',
+        'https://www.elpanoramadelasalud.com/wp-content/uploads/2021/07/proteina-vegana-600x534.jpg',
+        'https://laopinion.com/wp-content/uploads/sites/3/2019/11/shutterstock_696261196.jpg?quality=60&strip=all&w=1000',
+        'https://compro.com.co/rails/active_storage/representations/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBOEwxVUE9PSIsImV4cCI6bnVsbCwicHVyIjoiYmxvYl9pZCJ9fQ==--46a5e54ebd87d744b668f91316c53f073978364e/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaDdDRG9TY21WemFYcGxYM1J2WDJacGRGc0hhUUlnQTJrQ0lBTTZER052Ym5abGNuUkpJZ2hxY0djR09nWkZWRG9LYzJGMlpYSjdDRG9NY1hWaGJHbDBlV2xmT2dwemRISnBjRlE2RDJKaFkydG5jbTkxYm1SYkNHa0IvMmtCLzJrQi93PT0iLCJleHAiOm51bGwsInB1ciI6InZhcmlhdGlvbiJ9fQ==--f9b6205701877dad933126b17f2a1a57fbbba520/UROVITAL-NATURAL-FRESHLY-CAJ-X-60.jpg?locale=es',
+        'https://www.tiendanaturistacolombia.com/60-thickbox_default/purgante-natural8-capsulas-natural-freshly.jpg',
+    ];
+
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +34,12 @@ class ProductController extends Controller
      */
     public function index()
     {
-
+        $products = Product::all();
+        foreach ($products as $product) {
+            $code           = $product->image;
+            $product->image = ProductController::IMAGES[$code];
+        }
+        return view('products.index', ['products' => $products]);
     }
 
     /**
@@ -80,5 +106,11 @@ class ProductController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    private function getProducts()
+    {
+        $products = Product::select('products.id', 'products.name')->get();
+        return $products;
     }
 }
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -36,9 +36,9 @@
     @foreach ($products as $product)
         <div class="card">
             <div class="row">
-                <div class="col-sm-4">
-                    <img src="https://enciclopediaeconomica.com/wp-content/uploads/2021/10/icono-producto.jpg"
-                        class="card-img-top" alt="imagen producto">
+                <div class="col-sm-4 text-center">
+                    <img src="{{ $product->image }} " style="max-height: 380px; width: auto" class="card-img-top img-fluid"
+                        alt="imagen producto">
                 </div>
                 <div class="col-sm-8">
                     <div class="card-body">
@@ -48,10 +48,10 @@
                             <br>
                             Cantidad: {{ $product->quantity_and_measure }}
                         </p>
-                    </div>
-                    <div class="bottom text-end">
-                        <a href="#" class="btn button-show"><i class="fa-solid fa-eye"></i></a>
-                        <a href="#" class="btn button-cart"><i class="fa-solid fa-cart-shopping"></i></a>
+                        <div class="bottom text-end">
+                            <a href="#" class="btn button-show"><i class="fa-solid fa-eye"></i></a>
+                            <a href="#" class="btn button-cart"><i class="fa-solid fa-cart-shopping"></i></a>
+                        </div>
                     </div>
                 </div>
             </div>
