918c342d224a7a9d5fbae8267059cab580941a5a
camian0 <camian6@outlook.com>
2022-10-29 20:37:31 -0500

    model properties were changed

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,6 +9,20 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name',
+        'quantity_and_measure',
+        'description',
+        'expiration_date',
+        'lot_number',
+        'category_id',
+    ];
+
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
     public function category()
     {
         return $this->belongsTo(Category::class);
