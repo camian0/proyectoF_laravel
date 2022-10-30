d113e501d6006c3c693d96c3dce63990a2984cdb
camian0 <camian6@outlook.com>
2022-10-29 20:12:04 -0500

    route to products was added, product controller was added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +22,7 @@ Route::get('/', function () {
 //     return view('login');
 // });
 
-// Route::resource('/products', Produc);
+Route::resource('/products', ProductController::class);
 
 Route::middleware([
     'auth:sanctum',
